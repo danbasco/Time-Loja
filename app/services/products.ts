6564a1ad3ddde0d7c118e97d0db57a9d51b63ccb
Daniel Vasconcelos <79517613+danbasco@users.noreply.github.com>
2025-11-11 22:41:32 +0000

    feat: products

--- app/services/products.ts
+++ app/services/products.ts
@@ -1,24 +1,52 @@
 import type { CarProduct } from "~/types/product";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080/api";
+export const VITE_URL = import.meta.env.VITE_URL ?? (typeof window !== "undefined" ? window.location.origin : "http://localhost");
+const TOKEN_KEY = "token";
 
 export async function fetchAllProducts(): Promise<CarProduct[]> {
+
+  const token = localStorage.getItem(TOKEN_KEY);
+
   try {
-    const response = await fetch(`${API_BASE_URL}/products`);
+    const response = await fetch(`${VITE_URL}/api/veiculos`,
+      {method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      }
+      
+    );
+
+    console.log(response);
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
     const data = await response.json();
     return data;
+
   } catch (error) {
+
     console.error("Erro ao buscar produtos:", error);
+
     return [];
   }
 }
 
 export async function fetchProductById(id: string): Promise<CarProduct | null> {
+
+  const token = localStorage.getItem(TOKEN_KEY);
   try {
-    const response = await fetch(`${API_BASE_URL}/products/${id}`);
+    const response = await fetch(`${VITE_URL}/api/veiculos/${id}`,
+      
+      {method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+
+      }
+    );
     if (!response.ok) {
       if (response.status === 404) return null;
       throw new Error(`HTTP error! status: ${response.status}`);
